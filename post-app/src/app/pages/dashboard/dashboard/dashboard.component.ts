74cf03454bec912a4e0e8cb195785aae4a982c91
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-09-05 18:11:49 +0500

    dashboard count showed done

--- post-app/src/app/pages/dashboard/dashboard/dashboard.component.ts
+++ post-app/src/app/pages/dashboard/dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/shared/post-service/post.service';
+import { AlbumService } from 'src/app/shared/album-service/album.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  postCount: number = 0
+  albumCount: number = 0
+  photoCount: number = 0
+  commentCount: number = 0
+
+  constructor(private _postService: PostService, private _albumService: AlbumService) { }
 
   ngOnInit(): void {
+    this.getPostCount()
+    this.getCommentCount()
+    this.getAlbumCount()
+    this.getPhotoCount()
+  }
+
+  getPostCount() {
+    this._postService.getAllPost().subscribe(posts => this.postCount = posts.length)
+  }
+
+  getCommentCount() {
+    this._postService.getAllComments().subscribe(comments => this.commentCount = comments.length)
+  }
+
+  getAlbumCount() {
+    this._albumService.getAllAlbums().subscribe(albums => this.albumCount = albums.length)
+  }
+
+  getPhotoCount() {
+    this._albumService.getAlbumPictures().subscribe(pictures => this.photoCount = pictures.length)
   }
 
 }
