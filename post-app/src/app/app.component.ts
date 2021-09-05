e49e87f7652a1816ed0a77bfce572e603fa3c66e
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-09-05 18:21:58 +0500

    changes

--- post-app/src/app/app.component.ts
+++ post-app/src/app/app.component.ts
@@ -15,32 +15,7 @@ import {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // Sets initial value to true to show loading spinner on first load
-  loading = true
   isCollapsed = false;
 
-  constructor(private router: Router) {
-    this.router.events.subscribe((e: RouterEvent) => {
-      this.navigationInterceptor(e);
-    })
-  }
-
-  // Shows and hides the loading spinner during RouterEvent changes
-  navigationInterceptor(event: RouterEvent): void {
-    if (event instanceof NavigationStart) {
-      this.loading = true
-    }
-    if (event instanceof NavigationEnd) {
-      this.loading = false
-    }
-
-    // Set loading state to false in both of the below events to hide the spinner in case a request fails
-    if (event instanceof NavigationCancel) {
-      this.loading = false
-    }
-    if (event instanceof NavigationError) {
-      this.loading = false
-    }
-  }
 }
 
