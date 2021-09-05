53db561dc84afec7ae71e8d0bf396228773dc044
Aman Sultan Baig <amansultanbaig@gmail.com>
2021-09-05 13:52:53 +0500

    entire web design are done

--- post-app/src/app/pages/post/post-comments/post-comments.component.ts
+++ post-app/src/app/pages/post/post-comments/post-comments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { formatDistance, addDays } from 'date-fns/esm';
 
 @Component({
   selector: 'app-post-comments',
@@ -7,6 +8,96 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostCommentsComponent implements OnInit {
 
+  data = [
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 1))
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 2))
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 2))
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 2))
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 2))
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 2))
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 2))
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 2))
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 2))
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 2))
+    },
+    {
+      author: 'Han Solo',
+      avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+      content:
+        'We supply a series of design principles, practical patterns and high quality design resources' +
+        '(Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+      datetime: formatDistance(new Date(), addDays(new Date(), 2))
+    },
+  ];
   constructor() { }
 
   ngOnInit(): void {
--- post-app/src/app/pages/post/post.module.ts
+++ post-app/src/app/pages/post/post.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { PostListComponent } from './post-list/post-list.component';
 import { SinglePostViewComponent } from './single-post-view/single-post-view.component';
 import { PostCommentsComponent } from './post-comments/post-comments.component';
+import { NzCommentModule } from 'ng-zorro-antd/comment';
+import { NzListModule } from 'ng-zorro-antd/list';
 
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzGridModule } from 'ng-zorro-antd/grid';
@@ -26,6 +28,8 @@ const routes: Routes = [
     CommonModule,
     NzCardModule,
     NzGridModule,
+    NzCommentModule,
+    NzListModule,
     RouterModule.forChild(routes)
   ],
   exports: [PostListComponent, SinglePostViewComponent, PostCommentsComponent],
